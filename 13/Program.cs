493206a0c3bc8ede04f35b50755eebbc732f3970
Anna <nka2402@gmail.com>
2022-07-03 11:30:27 +0300

    Вторая домашка

---
+++ 13/Program.cs
@@ -0,0 +1,20 @@
+﻿/*
+Напишите программу, которая выводит третью цифру заданного числа 
+или сообщает, что третьей цифры нет.
+
+645 -> 5
+78 -> третьей цифры нет
+32679 -> 6
+*/
+
+Console.WriteLine("Введите число:");
+string numberStr = Console.ReadLine();
+int number = int.Parse(numberStr);
+if (number > 99)
+{
+    Console.WriteLine(numberStr.ToString()[2]);
+}
+else
+{
+    Console.WriteLine("Третьей цифры нет");
+}
